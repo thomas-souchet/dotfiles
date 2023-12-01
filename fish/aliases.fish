75633da8b08da2ac23796e4af849284b7109cc05
Thomas Souchet <thomassouchet22@gmail.com>
2023-12-01 11:03:43 +0100

    Add some new aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,6 +1,13 @@
-alias c clear
+alias c 'clear'
+alias xt 'exit'
+alias mv 'mv -i'
+alias reboot 'sudo /sbin/reboot'
 alias .. 'cd ..'
 alias ... 'cd ../..'
+
+function mkcd
+    mkdir $argv && cd $argv
+end
 
 # Directories
 alias cbut 'cd ~/Documents/ETUDES/IUT/BUT-Info/'
